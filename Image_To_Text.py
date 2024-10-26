269f19cfa160058fcf46b4ad05ed36a3ca443a6a
Gokurombus <ghoshabhinaba2007@gmail.com>
2024-10-26 19:33:37 +0530

    Image_To_Text.py

--- Image_To_Text.py
+++ Image_To_Text.py
@@ -1,11 +1,12 @@
 from tkinter.filedialog import *
 import pytesseract
+import random
 image = askopenfilename()
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\user\\Downloads\\tesseract"
 
 text = pytesseract.image_to_string(image)
 print(text)
-
-with open("text_file.txt" , "w+") as f:
+x=str(random.random())
+with open("text_file"+x+".txt" , "w+") as f:
     f.write(text)
